5bbe3166bca83921d0426b8d0917d7ea2215a8b4
migben <miguelangelben1990@gmail.com>
2020-01-24 11:03:17 -0500

    deliverables done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,55 @@
 class Product < ActiveRecord::Base
+    has_many :reviews
+    has_many :users, through: :reviews
+
+
+    def leave_review(user, star_rating, comment)
+
+        Review.create(comment: comment, star_rating: star_rating, user_id: user.id, product_id: self.id)
+
+    end
+
+    def print_all_reviews
+
+        Review.where(product_id: self.id)
+
+    end
+
+    def average_rating
+        
+        a1 = Review.where(product_id: self.id).sum(:star_rating)
+        a1 / Review.where(product_id: self.id).length.to_f
+
+    end
+
+#       t.string :comment
+#       t.integer :star_rating
+#       t.integer :user_id
+#       t.integer :product_id
+
     
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,32 @@ product4 = Product.create(name: "Ballpoint Pens", price: 2)
 product5 = Product.create(name: "Scotch Tape", price: 3)
 
 puts "Creating reviews..."
+
+
 # *****************************************************************
 # * TODO: create reviews! Remember, a review belongs to a product *
 # * and a review belongs to an user.                              *
 # *****************************************************************
+
+
+
 # Create Reviews Here
+
+
+
+
+r1 = Review.create(comment: "its realiable, been using it for long time", star_rating: 2, user_id: user1.id, product_id: product3.id)
+r2 = Review.create(comment: "Honestly, havent used it yet", star_rating: 4, user_id: user2.id, product_id: product2.id)
+r3 = Review.create(comment: "it doesnt work, dont buy", star_rating: 1, user_id: user2.id, product_id: product4.id)
+r4 = Review.create(comment: "need to order more for ma husband, its great", star_rating: 4, user_id: user3.id, product_id: product5.id)
+r5 = Review.create(comment: "ummm bought it by mistake, no regrets", star_rating: 5, user_id: user1.id, product_id: product1.id)
+r6 = Review.create(comment: "ummm bought it by mistake, no regrets", star_rating: 3, user_id: user3.id, product_id: product4.id)
+r7 = Review.create(comment: "I mistook this for something else, gave it to my brother", star_rating: 2, user_id: user1.id, product_id: product3.id)
+
+
+#       t.string :comment
+#       t.integer :star_rating
+#       t.integer :user_id
+#       t.integer :product_id
 
 puts "Seeding done!"
